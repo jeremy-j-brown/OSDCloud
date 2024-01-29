f70b179ca33f5d86b92ddafaae02a46e1110575c
Jeremy Brown <jbrown@ecog-acrin.org>
2024-01-29 11:56:58 -0500

    oobetasks

--- OOBE/OOBETasks.ps1
+++ OOBE/OOBETasks.ps1
@@ -6,28 +6,6 @@ If(!(Test-Path -Path $scriptFolderPath)) {
     New-Item -Path $scriptFolderPath -ItemType Directory -Force | Out-Null
 }
 
-$psscriptsini = @"
-`
-[Logon]
-0CmdLine=SendKeys.ps1
-0Parameters=
-1CmdLine=OOBE.ps1
-1Parameters=
-"@
-
-Out-File -FilePath "C:\WINDOWS\System32\GroupPolicy\User\Scripts\psscripts.ini" -InputObject $psscriptsini -Encoding ascii -Force
-
-$gpt = @"
-`[General]
-gPCUserExtensionNames=[{42B5FAAE-6536-11D2-AE5A-0000F87571E3}{40B66650-4972-11D1-A7CA-0000F87571E3}]
-Version=65536
-"@
-
-Out-File -FilePath “C:\WINDOWS\System32\GroupPolicy\gpt.ini” -InputObject $gpt -Encoding ascii -Force
-
-Set-ItemProperty -Path “C:\WINDOWS\System32\GroupPolicy” -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
-Set-ItemProperty -Path “C:\WINDOWS\System32\GroupPolicy\User\Scripts\psscripts.ini” -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
-
 $OOBEScript =@"
 `$Global:Transcript = "`$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OOBEScripts.log"
 Start-Transcript -Path (Join-Path "`$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" `$Global:Transcript) -ErrorAction Ignore | Out-Null
@@ -91,4 +69,52 @@ Out-File -FilePath $ScriptPathSendKeys -InputObject $SendKeysScript -Encoding as
 # Download ServiceUI.exe
 Write-Host -ForegroundColor Gray "Download ServiceUI.exe from GitHub Repo"
 Invoke-WebRequest https://github.com/jjblab/OSDPad/raw/main/Tools/ServiceUI64.exe -OutFile "C:\OSDCloud\ServiceUI.exe"
+
+#Create Scheduled Task for SendKeys with 15 seconds delay
+$TaskName = "Scheduled Task for SendKeys"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT15S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathSendKeys + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
+
+# Create Scheduled Task for OSDCloud post installation with 20 seconds delay
+$TaskName = "Scheduled Task for OSDCloud post installation"
+
+$ShedService = New-Object -comobject 'Schedule.Service'
+$ShedService.Connect()
+
+$Task = $ShedService.NewTask(0)
+$Task.RegistrationInfo.Description = $taskName
+$Task.Settings.Enabled = $true
+$Task.Settings.AllowDemandStart = $true
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa383987(v=vs.85).aspx
+$trigger = $task.triggers.Create(9) # 0 EventTrigger, 1 TimeTrigger, 2 DailyTrigger, 3 WeeklyTrigger, 4 MonthlyTrigger, 5 MonthlyDOWTrigger, 6 IdleTrigger, 7 RegistrationTrigger, 8 BootTrigger, 9 LogonTrigger
+$trigger.Delay = 'PT20S'
+$trigger.Enabled = $true
+
+$action = $Task.Actions.Create(0)
+$action.Path = 'C:\OSDCloud\ServiceUI.exe'
+$action.Arguments = '-process:RuntimeBroker.exe C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe ' + $ScriptPathOOBE + ' -NoExit'
+
+$taskFolder = $ShedService.GetFolder("\")
+# https://msdn.microsoft.com/en-us/library/windows/desktop/aa382577(v=vs.85).aspx
+$taskFolder.RegisterTaskDefinition($TaskName, $Task , 6, "SYSTEM", $NULL, 5)
 
